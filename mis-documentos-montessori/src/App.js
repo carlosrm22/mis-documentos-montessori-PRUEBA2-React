21e2cb126129b5bdefdd7a7b187ffcdb17bf29cb
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2024-07-21 18:54:06 -0600

    Se arreglan conflictos CSS

--- mis-documentos-montessori/src/App.js
+++ mis-documentos-montessori/src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Formulario from './components/Formulario';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar />
       <div className="container">
-        <h1 className="mt-5">Datos iniciales</h1>
-        <p>
-          Por favor, ingresa los datos iniciales para que podamos generar tus documentos. Asegúrate de llenar correctamente cada campo, ya que tu documento se generará exactamente como ingreses la información.
-        </p>
+
         <Formulario />
       </div>
       <Footer />
--- mis-documentos-montessori/src/components/Formulario.js
+++ mis-documentos-montessori/src/components/Formulario.js
@@ -78,59 +78,67 @@ function Formulario() {
     };
 
     return (
-        <form onSubmit={handleSubmit}>
-            <div className="p-3 mb-4 bg-white border rounded">
-                <h2 className="mt-4">Datos del alumno</h2>
-                <div className="form-group">
-                    <label>Apellidos del alumno: <span className="text-danger">*</span></label>
-                    <input type="text" className="form-control" name="apellidosAlumno" value={formData.apellidosAlumno} onChange={handleChange} required />
-                </div>
-                <div className="form-group">
-                    <label>Nombre(s) del alumno: <span className="text-danger">*</span></label>
-                    <input type="text" className="form-control" name="nombresAlumno" value={formData.nombresAlumno} onChange={handleChange} required />
-                </div>
-                <div className="form-group">
-                    <label>Fecha de nacimiento del alumno: <span className="text-danger">*</span></label>
-                    <input type="date" className="form-control" name="fechaNacimientoAlumno" value={formData.fechaNacimientoAlumno} onChange={handleChange} required />
-                </div>
-                <div className="form-group">
-                    <label>Edad del alumno: <span className="text-danger">*</span></label>
-                    <input type="number" className="form-control" name="edadAlumno" value={formData.edadAlumno} readOnly required />
-                </div>
+        <div className="container mt-5">
+            <div className="p-3 mb-4 bg-light rounded">
+                <h1 className="mt-5">Datos iniciales</h1>
+                <p>
+                    Por favor, ingresa los datos iniciales para que podamos generar tus documentos. Asegúrate de llenar correctamente cada campo, ya que tu documento se generará exactamente como ingreses la información.
+                </p>
             </div>
+            <form onSubmit={handleSubmit}>
+                <div className="p-3 mb-4 bg-white border rounded">
+                    <h2 className="mt-4">Datos del alumno</h2>
+                    <div className="form-group">
+                        <label>Apellidos del alumno: <span className="text-danger">*</span></label>
+                        <input type="text" className="form-control" name="apellidosAlumno" value={formData.apellidosAlumno} onChange={handleChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label>Nombre(s) del alumno: <span className="text-danger">*</span></label>
+                        <input type="text" className="form-control" name="nombresAlumno" value={formData.nombresAlumno} onChange={handleChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label>Fecha de nacimiento del alumno: <span className="text-danger">*</span></label>
+                        <input type="date" className="form-control" name="fechaNacimientoAlumno" value={formData.fechaNacimientoAlumno} onChange={handleChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label>Edad del alumno: <span className="text-danger">*</span></label>
+                        <input type="number" className="form-control" name="edadAlumno" value={formData.edadAlumno} readOnly required />
+                    </div>
+                </div>
 
-            <div className="p-3 mb-4 bg-white border rounded">
-                <h2 className="mt-4">Datos de los Padres o Tutores</h2>
-                <div className="form-group">
-                    <label>Apellidos del padre o tutor: <span className="text-danger">*</span></label>
-                    <input type="text" className="form-control" name="apellidosPadreTutor" value={formData.apellidosPadreTutor} onChange={handleChange} required />
+                <div className="p-3 mb-4 bg-white border rounded">
+                    <h2 className="mt-4">Datos de los Padres o Tutores</h2>
+                    <div className="form-group">
+                        <label>Apellidos del padre o tutor: <span className="text-danger">*</span></label>
+                        <input type="text" className="form-control" name="apellidosPadreTutor" value={formData.apellidosPadreTutor} onChange={handleChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label>Nombre(s) del padre o tutor: <span className="text-danger">*</span></label>
+                        <input type="text" className="form-control" name="nombresPadreTutor" value={formData.nombresPadreTutor} onChange={handleChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label>Apellidos de la madre o tutora: <span className="text-danger">*</span></label>
+                        <input type="text" className="form-control" name="apellidosMadreTutora" value={formData.apellidosMadreTutora} onChange={handleChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label>Nombre(s) de la madre o tutora: <span className="text-danger">*</span></label>
+                        <input type="text" className="form-control" name="nombresMadreTutora" value={formData.nombresMadreTutora} onChange={handleChange} required />
+                    </div>
+                    <div className="form-group">
+                        <label>Teléfono de contacto: <span className="text-danger">*</span></label>
+                        <input type="tel" className="form-control" name="telefonoContacto" value={formData.telefonoContacto} onChange={handleChange} required />
+                        <small className="form-text text-muted">Es posible que nos contactemos a este número vía WhatsApp para dar seguimiento a esta información.</small>
+                    </div>
+                    <div className="form-group">
+                        <label>Email de contacto: <span className="text-danger">*</span></label>
+                        <input type="email" className="form-control" name="emailContacto" value={formData.emailContacto} onChange={handleChange} required />
+                        <small className="form-text text-muted">Es posible que nos comuniquemos a este correo para dar seguimiento a esta información.</small>
+                    </div>
                 </div>
-                <div className="form-group">
-                    <label>Nombre(s) del padre o tutor: <span className="text-danger">*</span></label>
-                    <input type="text" className="form-control" name="nombresPadreTutor" value={formData.nombresPadreTutor} onChange={handleChange} required />
-                </div>
-                <div className="form-group">
-                    <label>Apellidos de la madre o tutora: <span className="text-danger">*</span></label>
-                    <input type="text" className="form-control" name="apellidosMadreTutora" value={formData.apellidosMadreTutora} onChange={handleChange} required />
-                </div>
-                <div className="form-group">
-                    <label>Nombre(s) de la madre o tutora: <span className="text-danger">*</span></label>
-                    <input type="text" className="form-control" name="nombresMadreTutora" value={formData.nombresMadreTutora} onChange={handleChange} required />
-                </div>
-                <div className="form-group">
-                    <label>Teléfono de contacto: <span className="text-danger">*</span></label>
-                    <input type="tel" className="form-control" name="telefonoContacto" value={formData.telefonoContacto} onChange={handleChange} required />
-                    <small className="form-text text-muted">Es posible que nos contactemos a este número vía WhatsApp para dar seguimiento a esta información.</small>
-                </div>
-                <div className="form-group">
-                    <label>Email de contacto: <span className="text-danger">*</span></label>
-                    <input type="email" className="form-control" name="emailContacto" value={formData.emailContacto} onChange={handleChange} required />
-                    <small className="form-text text-muted">Es posible que nos comuniquemos a este correo para dar seguimiento a esta información.</small>
-                </div>
-            </div>
 
-            <button type="submit" className="btn btn-primary">Guardar y continuar</button>
-        </form>
+                <button type="submit" className="btn btn-primary">Guardar y continuar</button>
+            </form>
+        </div>
     );
 }
 
