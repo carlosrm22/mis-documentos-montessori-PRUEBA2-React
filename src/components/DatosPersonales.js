074df719a6558991f4fc0780d2b5e3e3d8dcf51f
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2024-07-24 00:53:24 -0600

    se limpia código

--- src/components/DatosPersonales.js
+++ src/components/DatosPersonales.js
@@ -19,42 +19,26 @@ function DatosPersonales({ formData, setFormData }) {
                 con el aviso de privacidad que me ha sido dado a conocer previamente, mismo que de forma separada he
                 firmado para constancia y aceptación:</p>
             <form>
-                <div className="form-group">
-                    <label htmlFor="nombreAlumno">Nombre completo del alumno(a):</label>
-                    <input type="text" className="form-control" id="nombreAlumno" value={`${formData.nombresAlumno} ${formData.apellidosAlumno}`} readOnly />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="edadAlumno">Edad:</label>
-                    <input type="text" className="form-control" id="edadAlumno" value={formData.edadAlumno} readOnly />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="lugarNacimientoAlumno">Lugar de nacimiento: <span className="text-danger">*</span></label>
-                    <input type="text" className="form-control" id="lugarNacimientoAlumno" name="lugarNacimientoAlumno" value={formData.lugarNacimientoAlumno} onChange={handleChange} required />
-                    <small className="form-text text-muted">Ejemplo: Av. 2 No. 48 Colonia San Pedro de los Pinos, Benito Juárez, C.P. 03800, CDMX, México</small>
-                </div>
-                <div className="form-group">
-                    <label htmlFor="nombresMadre">Nombre(s) de la madre o tutor: <span className="text-danger">*</span></label>
-                    <input type="text" className="form-control" id="nombresMadre" name="nombresMadre" value={formData.nombresMadre} onChange={handleChange} required />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="apellidosMadre">Apellidos de la madre o tutor: <span className="text-danger">*</span></label>
-                    <input type="text" className="form-control" id="apellidosMadre" name="apellidosMadre" value={formData.apellidosMadre} onChange={handleChange} required />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="nombresPadre">Nombre(s) del padre o tutor: <span className="text-danger">*</span></label>
-                    <input type="text" className="form-control" id="nombresPadre" name="nombresPadre" value={formData.nombresPadre} onChange={handleChange} required />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="apellidosPadre">Apellidos del padre o tutor: <span className="text-danger">*</span></label>
-                    <input type="text" className="form-control" id="apellidosPadre" name="apellidosPadre" value={formData.apellidosPadre} onChange={handleChange} required />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="domicilioPadres">Domicilio particular de los padres o tutores: <span className="text-danger">*</span></label>
-                    <input type="text" className="form-control" id="domicilioPadres" name="domicilioPadres" value={formData.domicilioPadres} onChange={handleChange} required />
-                </div>
+                <FormGroup label="Nombre completo del alumno(a)" id="nombreAlumno" type="text" value={`${formData.nombresAlumno} ${formData.apellidosAlumno}`} readOnly />
+                <FormGroup label="Edad" id="edadAlumno" type="text" value={formData.edadAlumno} readOnly />
+                <FormGroup label="Lugar de nacimiento" id="lugarNacimientoAlumno" type="text" value={formData.lugarNacimientoAlumno} handleChange={handleChange} required smallText="Ejemplo: Av. 2 No. 48 Colonia San Pedro de los Pinos, Benito Juárez, C.P. 03800, CDMX, México" />
+                <FormGroup label="Nombre(s) de la madre o tutor" id="nombresMadre" type="text" value={formData.nombresMadre} handleChange={handleChange} required />
+                <FormGroup label="Apellidos de la madre o tutor" id="apellidosMadre" type="text" value={formData.apellidosMadre} handleChange={handleChange} required />
+                <FormGroup label="Nombre(s) del padre o tutor" id="nombresPadre" type="text" value={formData.nombresPadre} handleChange={handleChange} required />
+                <FormGroup label="Apellidos del padre o tutor" id="apellidosPadre" type="text" value={formData.apellidosPadre} handleChange={handleChange} required />
+                <FormGroup label="Domicilio particular de los padres o tutores" id="domicilioPadres" type="text" value={formData.domicilioPadres} handleChange={handleChange} required />
             </form>
         </div>
     );
 }
+
+// Componente reutilizable para un grupo de formulario
+const FormGroup = ({ label, id, type, value, handleChange, readOnly = false, smallText = null }) => (
+    <div className="form-group">
+        <label htmlFor={id}>{label}: <span className="text-danger">*</span></label>
+        <input type={type} className="form-control" id={id} value={value} onChange={handleChange} readOnly={readOnly} required />
+        {smallText && <small className="form-text text-muted">{smallText}</small>}
+    </div>
+);
 
 export default DatosPersonales;
