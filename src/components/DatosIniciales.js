657ab1c9eea0915f2fdb5494a005b14e78cd506c
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2024-07-24 10:31:42 -0600

    se instalan liberias fechas y formularios y se agrega Formik a DatosIniciales

--- src/components/DatosIniciales.js
+++ src/components/DatosIniciales.js
@@ -5,6 +5,8 @@ import Swal from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
 import { db } from '../firebaseConfig';
 import { collection, addDoc } from 'firebase/firestore';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
 
 /**
  * Componente principal para los datos iniciales.
@@ -15,69 +17,23 @@ import { collection, addDoc } from 'firebase/firestore';
 function DatosIniciales({ formData, setFormData }) {
     const navigate = useNavigate();
 
-    /**
-     * Maneja los cambios en los campos del formulario.
-     * @param {Object} e - El evento de cambio.
-     */
-    const handleChange = (e) => {
-        const { id, value } = e.target;
-        setFormData((prevFormData) => {
-            if (id === 'fechaNacimientoAlumno') {
-                const edad = calcularEdad(value);
-                return { ...prevFormData, [id]: value, edadAlumno: edad };
-            }
-            return { ...prevFormData, [id]: value };
-        });
-    };
+    const initialValues = formData;
 
-    /**
-     * Calcula la edad a partir de la fecha de nacimiento.
-     * @param {string} fechaNacimiento - La fecha de nacimiento.
-     * @returns {number} - La edad calculada.
-     */
-    const calcularEdad = (fechaNacimiento) => {
-        const hoy = new Date();
-        const fechaNac = new Date(fechaNacimiento);
-        let edad = hoy.getFullYear() - fechaNac.getFullYear();
-        const mes = hoy.getMonth() - fechaNac.getMonth();
-        if (mes < 0 || (mes === 0 && hoy.getDate() < fechaNac.getDate())) {
-            edad--;
-        }
-        return edad;
-    };
+    const validationSchema = Yup.object().shape({
+        apellidosAlumno: Yup.string().required('Requerido'),
+        nombresAlumno: Yup.string().required('Requerido'),
+        fechaNacimientoAlumno: Yup.date().required('Requerido'),
+        curpAlumno: Yup.string().required('Requerido'),
+        apellidosResponsable: Yup.string().required('Requerido'),
+        nombresResponsable: Yup.string().required('Requerido'),
+        telefonoContacto: Yup.string().required('Requerido'),
+        emailContacto: Yup.string().email('Email inválido').required('Requerido')
+    });
 
-    /**
-     * Valida el formulario antes de continuar.
-     * @param {Object} e - El evento de envío del formulario.
-     */
-    const validarFormulario = (e) => {
-        e.preventDefault();
-        if (!Object.values(formData).every(field => typeof field === 'string' ? field.trim() !== '' : field !== '')) {
-            mostrarAlerta('error', 'Ups, faltan datos', 'Por favor, completa todos los campos obligatorios.');
-        } else {
-            Swal.fire({
-                title: '¿Seguro que quieres guardar y continuar?',
-                text: "Revisa toda la información antes de continuar.",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#3085d6',
-                cancelButtonColor: '#d33',
-                confirmButtonText: 'Sí, guardar y continuar',
-                cancelButtonText: 'Revisar'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    guardarYContinuar();
-                }
-            });
-        }
-    };
-
-    /**
-     * Guarda la información en Firestore y muestra la alerta de éxito.
-     */
-    const guardarYContinuar = async () => {
+    const handleSubmit = async (values) => {
+        setFormData(values);
         try {
-            await addDoc(collection(db, 'datosIniciales'), formData);
+            await addDoc(collection(db, 'datosIniciales'), values);
             console.log('Datos guardados exitosamente');
             mostrarAlerta('success', 'Datos almacenados correctamente', 'Por favor, no actualice la página', false, 1500);
             navigate('/aviso-privacidad');
@@ -87,14 +43,6 @@ function DatosIniciales({ formData, setFormData }) {
         }
     };
 
-    /**
-     * Muestra una alerta de SweetAlert2.
-     * @param {string} icon - El ícono de la alerta.
-     * @param {string} title - El título de la alerta.
-     * @param {string} text - El texto de la alerta.
-     * @param {boolean} [showConfirmButton=true] - Si se debe mostrar el botón de confirmación.
-     * @param {number} [timer=null] - El tiempo en milisegundos para cerrar la alerta automáticamente.
-     */
     const mostrarAlerta = (icon, title, text, showConfirmButton = true, timer = null) => {
         Swal.fire({
             icon,
@@ -109,45 +57,65 @@ function DatosIniciales({ formData, setFormData }) {
         <div className="p-3 mb-4 bg-light rounded">
             <h1 className="mt-5">Datos iniciales</h1>
             <p>Ingresa los datos iniciales para generar tus documentos. Llena correctamente cada campo, ya que el documento se generará según la información ingresada. Por motivos de privacidad, no guardamos la información más que temporalmente, así que evita actualizar la página mientras completas tus datos. Gracias.</p>
-            <form id="miFormulario" onSubmit={validarFormulario}>
-                <SeccionDatosAlumno formData={formData} handleChange={handleChange} />
-                <SeccionDatosResponsable formData={formData} handleChange={handleChange} />
-                <button type="submit" className="btn btn-primary">Guardar y continuar</button>
-            </form>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={handleSubmit}
+            >
+                {({ values, handleChange }) => (
+                    <Form>
+                        <div className="p-3 mb-4 bg-white border rounded">
+                            <h2 className="mt-4">Datos del alumno</h2>
+                            <div className="form-group">
+                                <label htmlFor="apellidosAlumno">Apellidos del alumno: <span className="text-danger">*</span></label>
+                                <Field type="text" name="apellidosAlumno" className="form-control" />
+                                <ErrorMessage name="apellidosAlumno" component="div" className="text-danger" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="nombresAlumno">Nombre(s) del alumno: <span className="text-danger">*</span></label>
+                                <Field type="text" name="nombresAlumno" className="form-control" />
+                                <ErrorMessage name="nombresAlumno" component="div" className="text-danger" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="fechaNacimientoAlumno">Fecha de nacimiento del alumno: <span className="text-danger">*</span></label>
+                                <Field type="date" name="fechaNacimientoAlumno" className="form-control" />
+                                <ErrorMessage name="fechaNacimientoAlumno" component="div" className="text-danger" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="curpAlumno">CURP del alumno: <span className="text-danger">*</span></label>
+                                <Field type="text" name="curpAlumno" className="form-control" />
+                                <ErrorMessage name="curpAlumno" component="div" className="text-danger" />
+                            </div>
+                        </div>
+                        <div className="p-3 mb-4 bg-white border rounded">
+                            <h2 className="mt-4">Datos del responsable legal del alumno</h2>
+                            <div className="form-group">
+                                <label htmlFor="apellidosResponsable">Apellidos del responsable legal del alumno: <span className="text-danger">*</span></label>
+                                <Field type="text" name="apellidosResponsable" className="form-control" />
+                                <ErrorMessage name="apellidosResponsable" component="div" className="text-danger" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="nombresResponsable">Nombre(s) del responsable legal del alumno: <span className="text-danger">*</span></label>
+                                <Field type="text" name="nombresResponsable" className="form-control" />
+                                <ErrorMessage name="nombresResponsable" component="div" className="text-danger" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="telefonoContacto">Teléfono de contacto: <span className="text-danger">*</span></label>
+                                <Field type="tel" name="telefonoContacto" className="form-control" />
+                                <ErrorMessage name="telefonoContacto" component="div" className="text-danger" />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="emailContacto">Email de contacto: <span className="text-danger">*</span></label>
+                                <Field type="email" name="emailContacto" className="form-control" />
+                                <ErrorMessage name="emailContacto" component="div" className="text-danger" />
+                            </div>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Guardar y continuar</button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     );
 }
-
-// Componente para la sección de datos del alumno
-const SeccionDatosAlumno = ({ formData, handleChange }) => (
-    <div className="p-3 mb-4 bg-white border rounded">
-        <h2 className="mt-4">Datos del alumno</h2>
-        <FormGroup label="Apellidos del alumno" id="apellidosAlumno" type="text" value={formData.apellidosAlumno} handleChange={handleChange} />
-        <FormGroup label="Nombre(s) del alumno" id="nombresAlumno" type="text" value={formData.nombresAlumno} handleChange={handleChange} />
-        <FormGroup label="Fecha de nacimiento del alumno" id="fechaNacimientoAlumno" type="date" value={formData.fechaNacimientoAlumno} handleChange={handleChange} />
-        <FormGroup label="Edad del alumno" id="edadAlumno" type="number" value={formData.edadAlumno} readOnly />
-        <FormGroup label="CURP del alumno" id="curpAlumno" type="text" value={formData.curpAlumno} handleChange={handleChange} />
-    </div>
-);
-
-// Componente para la sección de datos del responsable legal
-const SeccionDatosResponsable = ({ formData, handleChange }) => (
-    <div className="p-3 mb-4 bg-white border rounded">
-        <h2 className="mt-4">Datos del responsable legal del alumno</h2>
-        <FormGroup label="Apellidos del responsable legal del alumno" id="apellidosResponsable" type="text" value={formData.apellidosResponsable} handleChange={handleChange} />
-        <FormGroup label="Nombre(s) del responsable legal del alumno" id="nombresResponsable" type="text" value={formData.nombresResponsable} handleChange={handleChange} />
-        <FormGroup label="Teléfono de contacto" id="telefonoContacto" type="tel" value={formData.telefonoContacto} handleChange={handleChange} smallText="Es posible que nos contactemos a este número vía WhatsApp para dar seguimiento a esta información." />
-        <FormGroup label="Email de contacto" id="emailContacto" type="email" value={formData.emailContacto} handleChange={handleChange} smallText="Es posible que nos comuniquemos a este correo para dar seguimiento a esta información." />
-    </div>
-);
-
-// Componente reutilizable para un grupo de formulario
-const FormGroup = ({ label, id, type, value, handleChange, readOnly = false, smallText = null }) => (
-    <div className="form-group">
-        <label htmlFor={id}>{label}: <span className="text-danger">*</span></label>
-        <input type={type} className="form-control" id={id} value={value} onChange={handleChange} readOnly={readOnly} required />
-        {smallText && <small className="form-text text-muted">{smallText}</small>}
-    </div>
-);
 
 export default DatosIniciales;
