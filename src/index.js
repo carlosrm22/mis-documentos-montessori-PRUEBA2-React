2c17fb0d2ee7144255bdb8497d9cc0c25130e5d5
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2024-07-25 02:13:28 -0600

    actualiza index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 // src/index.js
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import App from './containers/App'; // Actualizamos la referencia a App
+import App from './containers/App';
+import './styles/App.css';
 
 // Crear la raíz de React usando createRoot
 const container = document.getElementById('root');
