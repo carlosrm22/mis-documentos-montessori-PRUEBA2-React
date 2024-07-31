29a48d7da95ea22e44ab12fbde578a29edd672f3
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2024-07-30 21:13:13 -0600

    seguridad

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -7,13 +7,13 @@ import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD32ePJYpLTPK-wYS1_cKMTv28h_Wjk5TM",
-    authDomain: "montessori-728c0.firebaseapp.com",
-    projectId: "montessori-728c0",
-    storageBucket: "montessori-728c0.appspot.com",
-    messagingSenderId: "583557147412",
-    appId: "1:583557147412:web:f7477f93f1988f8185b001",
-    measurementId: "G-E80WFVF6DJ"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Inicializar Firebase
