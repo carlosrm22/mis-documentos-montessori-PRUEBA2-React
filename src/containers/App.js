479508537cdb77975ab6fbed447b7c15edb91caa
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2024-07-31 01:04:45 -0600

    se agregó GlobalStateProvider a App.js y mejoró validationSchemas.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,4 @@
-// src/containers/App.js
-
-import React, { useState } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../utils/firebaseConfig';
@@ -14,109 +12,43 @@ import Login from '../components/Login';
 import Dashboard from '../components/Dashboard';
 import ContratoReglamento from '../components/ContratoReglamento';
 import Bienvenida from '../components/Bienvenida';
-import { saveData, subirPDFaFirebase } from '../services/firebaseService';
-import { mostrarAvisoDatos, mostrarAvisoPDF, mostrarAlertaExito, mostrarAlertaError, mostrarAvisoRevisar } from '../utils/sweetAlertUtils';
-import { generarPDF } from '../utils/pdfUtils';
+import { GlobalStateProvider } from '../utils/GlobalState';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../styles/App.css';
-import { registrarEvento } from '../utils/firebaseConfig'; // Asegúrate de importar `registrarEvento`
 
 /**
  * Componente principal de la aplicación.
  */
 function App() {
-  // Estado inicial del formulario
-  const [formData, setFormData] = useState({
-    apellidosAlumno: '',
-    nombresAlumno: '',
-    fechaNacimientoAlumno: '',
-    edadAlumno: '',
-    curpAlumno: '',
-    apellidosResponsable: '',
-    nombresResponsable: '',
-    telefonoContacto: '',
-    emailContacto: '',
-    lugarNacimientoAlumno: '',
-    nombresMadre: '',
-    apellidosMadre: '',
-    nombresPadre: '',
-    apellidosPadre: '',
-    domicilioPadres: '',
-    nivelEducativo: ''
-  });
-
-  // Función para obtener la fecha actual
-  const getFechaActual = () => {
-    const fecha = new Date();
-    const meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"];
-    const dia = fecha.getDate();
-    const mes = meses[fecha.getMonth()];
-    const año = fecha.getFullYear();
-    return `A los días ${dia} del mes de ${mes} del año ${año}`;
-  };
-
-  // Función para guardar datos iniciales
-  const handleGuardarDatosIniciales = async () => {
-    try {
-      await saveData('datosIniciales', formData);
-      registrarEvento('guardar_datos_iniciales', { usuario: auth.currentUser.uid });
-      alert('Datos guardados exitosamente');
-    } catch (error) {
-      console.error('Error guardando datos:', error);
-      alert('Error al guardar los datos');
-    }
-  };
-
-  // Función para manejar la generación y subida del PDF
-  const handleGenerarYSubirPDF = async (inputId, storagePath, callback) => {
-    const result = await mostrarAvisoPDF();
-
-    if (result.isConfirmed) {
-      const pdfBlob = await generarPDF(inputId);
-      await subirPDFaFirebase(pdfBlob, storagePath);
-
-      // Descargar el PDF en la computadora del usuario con el nombre específico
-      const nombreAlumno = formData.nombresAlumno.split(' ').join('-') + '-' + formData.apellidosAlumno.split(' ').join('-');
-      const nombreArchivo = `${inputId}-${nombreAlumno}.pdf`;
-
-      const link = document.createElement('a');
-      link.href = URL.createObjectURL(pdfBlob);
-      link.download = nombreArchivo;
-      link.click();
-
-      if (callback) {
-        callback();
-      }
-    }
-  };
-
   const [user] = useAuthState(auth);
 
   return (
-    <Router>
-      <Navbar />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<Bienvenida />} />
-          <Route path="/datos-iniciales" element={<DatosIniciales formData={formData} setFormData={setFormData} onSave={handleGuardarDatosIniciales} />} />
-          <Route path="/aviso-privacidad" element={<AvisoPrivacidad formData={formData} getFechaActual={getFechaActual} onGenerarYSubirPDF={handleGenerarYSubirPDF} />} />
-          <Route path="/datos-personales" element={<DatosPersonales formData={formData} />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/contrato-reglamento" element={<ContratoReglamento formData={formData} nivelEducativo={formData.nivelEducativo} />} />
-          <Route
-            path="/dashboard"
-            element={user ? <Dashboard formData={formData} setFormData={setFormData} /> : <Navigate to="/login" />}
-          />
-          <Route
-            path="*"
-            element={<Navigate to={user ? "/dashboard" : "/login"} />}
-          />
-        </Routes>
-      </div>
-      <Footer />
-    </Router>
+    <GlobalStateProvider>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Bienvenida />} />
+            <Route path="/datos-iniciales" element={<DatosIniciales />} />
+            <Route path="/aviso-privacidad" element={<AvisoPrivacidad />} />
+            <Route path="/datos-personales" element={<DatosPersonales />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/contrato-reglamento" element={<ContratoReglamento />} />
+            <Route
+              path="/dashboard"
+              element={user ? <Dashboard /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="*"
+              element={<Navigate to={user ? "/dashboard" : "/login"} />}
+            />
+          </Routes>
+        </div>
+        <Footer />
+      </Router>
+    </GlobalStateProvider>
   );
 }
 
--- src/utils/validationSchemas.js
+++ src/utils/validationSchemas.js
@@ -1,30 +1,13 @@
-// src/utils/validationSchemas.js
-
 import * as yup from 'yup';
-import { parsePhoneNumberFromString } from 'libphonenumber-js';
 
-const isPhoneNumberValid = (value) => {
-    if (!value) return false;
-    const phoneNumber = parsePhoneNumberFromString(value, 'MX');
-    return phoneNumber && phoneNumber.isValid();
-};
-
-// Esquema de validación para los datos iniciales
 export const datosInicialesValidationSchema = yup.object().shape({
-    apellidosAlumno: yup.string().required('Los apellidos del alumno son requeridos'),
-    nombresAlumno: yup.string().required('El nombre del alumno es requerido'),
-    fechaNacimientoAlumno: yup.date()
-        .max(new Date(), "La fecha de nacimiento no puede ser en el futuro")
-        .min(new Date(new Date().setFullYear(new Date().getFullYear() - 100)), "La edad del alumno no es válida")
-        .required('La fecha de nacimiento es requerida'),
-    curpAlumno: yup.string().matches(/^[A-Z0-9]{18}$/, 'La CURP no es válida').required('La CURP del alumno es requerida'),
-    apellidosResponsable: yup.string().required('Los apellidos del responsable son requeridos'),
-    nombresResponsable: yup.string().required('El nombre del responsable es requerido'),
-    telefonoContacto: yup.string()
-        .test('is-valid-phone', 'El teléfono de contacto no es válido', isPhoneNumberValid)
-        .required('El teléfono de contacto es requerido'),
-    emailContacto: yup.string()
-        .email('El email debe ser válido')
-        .matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'El email debe tener un dominio válido')
-        .required('El email de contacto es requerido')
+    apellidosAlumno: yup.string().required('Este campo es obligatorio'),
+    nombresAlumno: yup.string().required('Este campo es obligatorio'),
+    fechaNacimientoAlumno: yup.date().required('Este campo es obligatorio').nullable(),
+    curpAlumno: yup.string().matches(/^([A-Z]{4}\d{6}[A-Z]{6}\d{2})$/, 'CURP inválido').required('Este campo es obligatorio'),
+    apellidosResponsable: yup.string().required('Este campo es obligatorio'),
+    nombresResponsable: yup.string().required('Este campo es obligatorio'),
+    telefonoContacto: yup.string().matches(/^\d{10}$/, 'Número de teléfono inválido').required('Este campo es obligatorio'),
+    emailContacto: yup.string().email('Correo electrónico inválido').required('Este campo es obligatorio'),
+    nivelEducativo: yup.string().required('Este campo es obligatorio')
 });
