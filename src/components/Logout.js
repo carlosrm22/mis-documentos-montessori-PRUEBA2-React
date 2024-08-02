e8fee8485b0c593085ea419f8cd2b81ed619956c
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2024-08-02 01:31:53 -0600

    build

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -3,7 +3,14 @@
 import React from 'react';
 import { logout } from '../services/firebaseService';
 
+/**
+ * Componente para cerrar sesión.
+ * Llama a la función de logout del servicio de Firebase.
+ */
 const Logout = () => {
+    /**
+     * Maneja el evento de clic para cerrar sesión.
+     */
     const handleLogout = () => {
         logout();
     };
