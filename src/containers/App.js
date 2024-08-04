9f8bd51f028771830da8c2430c45de8ad16e5d9b
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2024-08-04 04:41:30 -0600

    feat: Refactorizar componente App para mejorar rendimiento y manejo de estado global

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,8 +1,5 @@
-// src/containers/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '../utils/firebaseConfig';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import DatosIniciales from '../components/DatosIniciales';
@@ -13,49 +10,41 @@ import Login from '../components/Login';
 import Dashboard from '../components/Dashboard';
 import ContratoReglamento from '../components/ContratoReglamento';
 import Bienvenida from '../components/Bienvenida';
-import { useLoading } from '../utils/LoadingContext';
 import LoadingSpinner from '../components/LoadingSpinner';
+import { useGlobalState } from '../utils/GlobalState';
+import useAuth from '../utils/useAuth';
+import useLoading from '../utils/useLoading';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import '../styles/App.css';
 import '../styles/LoadingSpinner.css';
 
 function App() {
-  const [user] = useAuthState(auth);
+  const { user, loading } = useGlobalState();
+  useAuth(); // Custom hook for handling auth
+  const setLoading = useLoading(); // Custom hook for setting loading state
 
   return (
     <Router>
-      <LoadingConsumer>
-        {({ loading, setLoading }) => (
-          <>
-            {loading && <LoadingSpinner />}
-            <Navbar />
-            <div className="container">
-              <Routes>
-                <Route path="/" element={<Bienvenida />} />
-                <Route path="/datos-iniciales" element={<DatosIniciales setLoading={setLoading} />} />
-                <Route path="/aviso-privacidad" element={<AvisoPrivacidad setLoading={setLoading} />} />
-                <Route path="/datos-personales" element={<DatosPersonales />} />
-                <Route path="/register" element={user ? <Navigate to="/dashboard" /> : <Register />} />
-                <Route path="/login" element={user ? <Navigate to="/dashboard" /> : <Login />} />
-                <Route path="/contrato-reglamento" element={<ContratoReglamento />} />
-                <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
-              </Routes>
-            </div>
-            <Footer />
-          </>
-        )}
-      </LoadingConsumer>
+      <>
+        {loading && <LoadingSpinner />}
+        <Navbar />
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<Bienvenida />} />
+            <Route path="/datos-iniciales" element={<DatosIniciales />} />
+            <Route path="/aviso-privacidad" element={<AvisoPrivacidad />} />
+            <Route path="/datos-personales" element={<DatosPersonales />} />
+            <Route path="/register" element={user ? <Navigate to="/dashboard" /> : <Register />} />
+            <Route path="/login" element={user ? <Navigate to="/dashboard" /> : <Login />} />
+            <Route path="/contrato-reglamento" element={<ContratoReglamento />} />
+            <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+          </Routes>
+        </div>
+        <Footer />
+      </>
     </Router>
   );
 }
-
-const LoadingConsumer = ({ children }) => {
-  const context = useLoading();
-  if (context === undefined) {
-    throw new Error('useLoading must be used within a LoadingProvider');
-  }
-  return children(context);
-};
 
 export default App;
