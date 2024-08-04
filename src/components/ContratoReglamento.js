108e868ddbd7202bf1211131bac2e186ab2e9331
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2024-08-04 15:53:58 -0600

    useCallback par fetchData, manejo d carga y errores setLoading(), try-catch-finally, react.memo

--- src/components/ContratoReglamento.js
+++ src/components/ContratoReglamento.js
@@ -1,14 +1,34 @@
-// src/components/ContratoReglamento.js
+import React, { useEffect, useCallback } from 'react';
+import { useGlobalState, useGlobalDispatch } from '../utils/GlobalState';
+import { cargarDatosIniciales } from '../utils/dataUtils';
+import useAuth from '../utils/useAuth';
+import useLoading from '../utils/useLoading';
 
-import React from 'react';
-import { useGlobalState } from '../utils/GlobalState';
-
-/**
- * Componente para la sección de Contrato y Reglamento.
- * Muestra el contrato de prestación de servicios educativos.
- */
 const ContratoReglamento = () => {
     const { formData } = useGlobalState();
+    const dispatch = useGlobalDispatch();
+    const setLoading = useLoading();
+    const { user, authLoading } = useAuth();
+
+    const fetchData = useCallback(async () => {
+        setLoading(true);
+        try {
+            if (!formData) {
+                const data = await cargarDatosIniciales(dispatch);
+                dispatch({ type: 'SET_FORM_DATA', payload: data });
+            }
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }, [dispatch, formData, setLoading]);
+
+    useEffect(() => {
+        if (authLoading) return;
+        if (!user) return;
+        fetchData();
+    }, [fetchData, user, authLoading]);
 
     if (!formData || !formData.nombresResponsable) {
         return <div>Cargando datos...</div>;
@@ -18,11 +38,9 @@ const ContratoReglamento = () => {
 
     return (
         <div className="container mt-5">
-            {/* Título del contrato */}
             <h1 className="text-center p-4">CONTRATO DE PRESTACIÓN DE SERVICIOS EDUCATIVOS</h1>
             <h2 className="text-end">CICLO ESCOLAR 2021-2022</h2>
             <h3 className="text-end p-4">NIVEL EDUCATIVO {nivelEducativo}</h3>
-            {/* Contenido del contrato */}
             <p>
                 CONTRATO DE PRESTACIÓN DE SERVICIOS EDUCATIVOS QUE CELEBRAN POR UNA PARTE LA ASOCIACIÓN
                 MONTESSORI DE MÉXICO A.C. Y A QUIEN EN LO SUCESIVO SE LE DENOMINARA “EL PRESTADOR” CON DOMICILIO
@@ -36,4 +54,4 @@ const ContratoReglamento = () => {
     );
 };
 
-export default ContratoReglamento;
+export default React.memo(ContratoReglamento);
