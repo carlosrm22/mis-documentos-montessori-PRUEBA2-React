b2bbdb981bd1d5608e123218da1f79cb6c2879df
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2024-08-04 02:35:19 -0600

    mantener estado de autenticación SET_USER

---
+++ src/utils/authUtils.js
@@ -0,0 +1,22 @@
+// src/utils/authUtils.js
+import { auth } from './firebaseConfig';
+import { onAuthStateChanged } from 'firebase/auth';
+
+/**
+ * Verifica el estado de autenticación del usuario.
+ * @param {Function} dispatch - Función para despachar acciones al estado global.
+ * @returns {Function} unsubscribe - Función para cancelar la suscripción a los cambios en el estado de autenticación.
+ */
+export const verificarAutenticacion = (dispatch) => {
+    try {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            const actionType = currentUser ? 'SET_USER' : 'SET_USER';
+            const payload = currentUser || null;
+            dispatch({ type: actionType, payload });
+        });
+
+        return unsubscribe;
+    } catch (error) {
+        console.error('Error al verificar la autenticación:', error);
+    }
+};
