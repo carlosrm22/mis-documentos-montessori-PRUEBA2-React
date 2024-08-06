ed6aa90230d77da5ec69f306ac7aa515a2e1962d
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2024-08-05 21:10:53 -0600

    se quita la importanción redundante de App.css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-// src/index.js
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import '@fortawesome/fontawesome-free/css/all.min.css';
@@ -7,20 +6,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './containers/App';
 import { GlobalProvider } from './utils/GlobalState';
-import { LoadingProvider } from './utils/LoadingContext';
 import './styles/App.css';
 
-// Crear la raíz de React usando createRoot
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-// Renderizar la aplicación en la raíz
 root.render(
   <React.StrictMode>
-    <LoadingProvider>
-      <GlobalProvider>
-        <App />
-      </GlobalProvider>
-    </LoadingProvider>
+    <GlobalProvider>
+      <App />
+    </GlobalProvider>
   </React.StrictMode>
 );
