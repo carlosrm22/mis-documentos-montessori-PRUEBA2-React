1aa63208a6007761ae5411c1762dfc23bd6e9e60
carlosrm22 <120687366+carlosrm22@users.noreply.github.com>
2024-08-06 17:52:52 -0600

    Centralizar acciones

---
+++ src/utils/actionTypes.js
@@ -0,0 +1,5 @@
+// src/utils/actionTypes.js
+export const SET_FORM_DATA = 'SET_FORM_DATA';
+export const SET_USER = 'SET_USER';
+export const SET_LOADING = 'SET_LOADING';
+export const SET_ERROR = 'SET_ERROR';
